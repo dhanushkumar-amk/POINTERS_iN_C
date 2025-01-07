af8fe306b4e0d7463ac0ff0ee094287b14288a28
Dhanushkumar G <145154541+dhanushkumar-amk@users.noreply.github.com>
2025-01-08 05:14:38 +0530

    Create Program1.c

---
+++ Program1.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   
+   int a;
+   int *p;
+   p = &a; // &a is a adresss of a
+   printf("%d\n", p);
+   printf("%d\n", *p);
+
+    return 0;
+}
